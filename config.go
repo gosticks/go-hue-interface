815895eca74182b5db1a60476a12fa4443692812
Wlad Meixner <wlad.meixner@protonmail.com>
2019-02-09 16:49:01 +0100

    extended config

--- config.go
+++ config.go
@@ -2,7 +2,8 @@ package main
 
 // Config hue api config
 type Config struct {
-	Username   string `yaml:name`
-	Password   string `yaml:userpassword`
-	BridgeAddr string `yaml:bridgeAddress`
+	Username         string `yaml:name`
+	Password         string `yaml:userpassword`
+	BridgeAddr       string `yaml:bridgeAddress`
+	BridgeAddrScheme string `yaml:bridgeAddressScheme`
 }
