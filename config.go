9019b30330b0009da17211f0b3de000bcbc2e9a2
Your Name <you@example.com>
2019-02-09 16:56:58 +0100

    bla

--- config.go
+++ config.go
@@ -1,7 +1,39 @@
 package main
 
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
 // Config hue api config
 type Config struct {
 	Username string `yaml:name`
 	Password string `yaml:userpassword`
+}
+
+// createNewUser will create a new user. This should be called only of there's none in the yaml config.
+func (c *Config) createNewUser() {
+	// TODO: read/create the url
+	url := "http://192.168.178.46/api"
+
+    var reqBody = []byte(`{"devicetype": "go-hue-interface#Philips hue"}`)
+    req, err := http.NewRequest("POST", url, bytes.NewBuffer(reqBody))
+	
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("response Status:", resp.Status)
+    fmt.Println("response Headers:", resp.Header)
+    body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+
+    json.Unmarshal([]byte(str), &res)
+    fmt.Println(res)
+    fmt.Println(res.Fruits[0])
 }
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const VERSION = "0.1.2"
 
