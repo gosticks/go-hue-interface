6c95d8cee4731655e9ab54ac58e45838c0fb5a25
Wlad Meixner <wlad.meixner@protonmail.com>
2019-02-09 16:53:38 +0100

    add post to bridge method

--- bridge.go
+++ bridge.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 )
 
+// Bridge is the hue bridge interface
 type Bridge struct {
 	Config *Config
 }
@@ -15,16 +16,21 @@ func (b *Bridge) postToBridge(endpoint string, payload interface{}) (interface{}
 	if errMarhshal != nil {
 		return nil, errMarhshal
 	}
-
-	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(data))
+	uri := b.getBridgeAPIURI() + endpoint
+	req, err := http.NewRequest(http.MethodPost, uri, bytes.NewBuffer(data))
 	if err != nil {
 		return nil, err
 	}
 
 	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
 	res, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	return res, nil
+}
+
+func (b *Bridge) getBridgeAPIURI() string {
+	return b.Config.BridgeAddrScheme + "://" + b.Config.BridgeAddr + "/api/" + b.Config.Username
 }
