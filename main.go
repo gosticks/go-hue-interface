833a1bb880e694a4be5ecc5926cd8acc9c7332b2
Wlad Meixner <wlad.meixner@protonmail.com>
2019-02-11 08:59:09 +0100

    cleanup

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package hue
 
 import (
 	"fmt"
-	"time"
 )
 
 const VERSION = "0.1.2"
@@ -25,33 +24,4 @@ func main() {
 	// 	fmt.Println("[ERROR]" + errCom.Error())
 	// }
 	//fmt.Println(test)
-}
-
-func strobeLight(b *Bridge, id string) {
-	ticker := time.NewTicker(200 * time.Millisecond)
-	quit := make(chan struct{})
-	go func() {
-		state := false
-		for {
-			select {
-			case <-ticker.C:
-				resp, errCom := b.ToggleLight(id, state)
-				if errCom != nil {
-					fmt.Println("[ERROR]" + errCom.Error())
-					ticker.Stop()
-					return
-				}
-				if resp.Error != nil {
-					fmt.Println("[ERROR]" + resp.Error.String())
-					ticker.Stop()
-					return
-				}
-				state = !state
-				// do stuff
-			case <-quit:
-				ticker.Stop()
-				return
-			}
-		}
-	}()
 }
